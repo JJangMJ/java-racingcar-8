f3c32bc8c114fca4f7d837bc6b8e4dc2134b5ee3
JMJ <ghtntkdnsk@naver.com>
2025-10-25 17:29:00 +0900

    feat: 경주할 자동차의 이름이 중복인 경우 예외 처리

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -2,7 +2,8 @@ package racingcar.exception;
 
 public enum ErrorMessage {
     RACING_CAR_NAME_CANNOT_BE_BLANK("자동차 이름은 공백일 수 없습니다."),
-    RACING_CAR_NAME_LENGTH_SHOULD_BE_1_TO_5("자동차 이름은 1~5자 이내여야 합니다.");
+    RACING_CAR_NAME_LENGTH_SHOULD_BE_1_TO_5("자동차 이름은 1~5자 이내여야 합니다."),
+    RACING_CAR_NAME_CANNOT_BE_DUPLICATED("자동차 이름은 중복될 수 없습니다.");
 
     private final String errorMessage;
 
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,15 +2,26 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import java.util.Set;
+import racingcar.exception.ErrorMessage;
 
 public class InputView {
     public List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
-        return List.of(Console.readLine().split(","));
+        List<String> carNames = List.of(Console.readLine().split(","));
+        validateDuplicateName(carNames);
+        return carNames;
     }
 
     public int inputRoundCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         return Integer.parseInt(Console.readLine());
+    }
+
+    private void validateDuplicateName(List<String> carNames) {
+        Set<String> uniqueNames = Set.copyOf(carNames);
+        if (uniqueNames.size() != carNames.size()) {
+            throw new IllegalArgumentException(ErrorMessage.RACING_CAR_NAME_CANNOT_BE_DUPLICATED.getErrorMessage());
+        }
     }
 }
