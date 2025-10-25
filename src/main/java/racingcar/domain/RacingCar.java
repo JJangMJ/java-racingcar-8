5e3551c06b789bcf537827ec5175dc8d99f0a565
JMJ <ghtntkdnsk@naver.com>
2025-10-25 13:53:05 +0900

    refactor: 자동차 이동 조건을 판별하는 메서드를 RacingCar로 위치 변경

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -3,6 +3,8 @@ package racingcar.domain;
 public class RacingCar {
     private static final int MAX_NAME_LENGTH = 5;
     private static final int START_POSITION = 0;
+    private static final int MOVE_MIN_VALUE = 4;
+
     private final String name;
     private int position;
 
@@ -12,16 +14,9 @@ public class RacingCar {
         this.position = START_POSITION;
     }
 
-    public void move() {
-        this.position ++;
-    }
-
-    private void validateRacingCarName(String name) {
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
-        }
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("자동차 이름은 1~5자 이내여야 합니다.");
+    public void move(int randomValue) {
+        if (canMove(randomValue)) {
+            this.position ++;
         }
     }
 
@@ -31,5 +26,18 @@ public class RacingCar {
 
     public int getPosition() {
         return position;
+    }
+
+    private boolean canMove(int randomValue) {
+        return randomValue >= MOVE_MIN_VALUE;
+    }
+
+    private void validateRacingCarName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+        }
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 1~5자 이내여야 합니다.");
+        }
     }
 }
--- src/main/java/racingcar/domain/RacingRound.java
+++ src/main/java/racingcar/domain/RacingRound.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class RacingRound {
     private static final int RANDOM_NUMBER_MIN = 0;
     private static final int RANDOM_NUMBER_MAX = 9;
-    private static final int MOVE_MIN_VALUE = 4;
     private final List<RacingCar> racingCars;
 
     public RacingRound(List<RacingCar> racingCars) {
@@ -16,13 +15,7 @@ public class RacingRound {
     public void start() {
         racingCars.forEach(racingCar -> {
             int randomValue = Randoms.pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_MAX);
-            if (canMove(randomValue)) {
-                racingCar.move();
-            }
+            racingCar.move(randomValue);
         });
-    }
-
-    private boolean canMove(int randomValue) {
-        return randomValue >= MOVE_MIN_VALUE;
     }
 }
