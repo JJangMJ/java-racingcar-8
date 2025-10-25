13893555ae285379eb5312a3b89c4f1c66bfb1cf
JMJ <ghtntkdnsk@naver.com>
2025-10-25 13:04:02 +0900

    refactor: 자동차가 움직일 수 있는 조건을 판별하는 메서드 추가

--- src/main/java/racingcar/domain/RacingRound.java
+++ src/main/java/racingcar/domain/RacingRound.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class RacingRound {
     private static final int RANDOM_NUMBER_MIN = 0;
     private static final int RANDOM_NUMBER_MAX = 9;
+    private static final int MOVE_MIN_VALUE = 4;
     private final List<RacingCar> racingCars;
 
     public RacingRound(List<RacingCar> racingCars) {
@@ -15,9 +16,13 @@ public class RacingRound {
     public void start() {
         racingCars.forEach(racingCar -> {
             int randomValue = Randoms.pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_MAX);
-            if (randomValue >= 4) {
+            if (canMove(randomValue)) {
                 racingCar.move();
             }
         });
+    }
+
+    private boolean canMove(int randomValue) {
+        return randomValue >= MOVE_MIN_VALUE;
     }
 }
