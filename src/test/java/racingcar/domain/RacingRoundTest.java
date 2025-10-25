a31854acaca0a074cc491fc25d6eb35f890797f5
JMJ <ghtntkdnsk@naver.com>
2025-10-25 13:59:55 +0900

    test: RacingRound에 대한 테스트 코드 구현

--- src/test/java/racingcar/domain/RacingRoundTest.java
+++ src/test/java/racingcar/domain/RacingRoundTest.java
@@ -1,4 +1,34 @@
 package racingcar.domain;
 
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RacingRoundTest {
+    @Test
+    void 라운드를_시작하면_각_자동차의_위치는_0_또는_1() {
+        List<RacingCar> racingCars = List.of(new RacingCar("pobi"), new RacingCar("woni"));
+        RacingRound round = new RacingRound(racingCars);
+
+        round.start();
+
+        racingCars.forEach(car ->
+                Assertions.assertThat(car.getPosition()).isBetween(0, 1)
+        );
+    }
+
+    @Test
+    void 라운드를_N번_실행하면_각_자동차의_위치는_0에서_N_사이() {
+        int roundCount = 10;
+        List<RacingCar> racingCars = List.of(new RacingCar("pobi"), new RacingCar("woni"));
+
+        for (int i = 0; i < roundCount; i++) {
+            RacingRound racingRound = new RacingRound(racingCars);
+            racingRound.start();
+        }
+
+        racingCars.forEach(car ->
+                Assertions.assertThat(car.getPosition()).isBetween(0, roundCount)
+        );
+    }
 }
