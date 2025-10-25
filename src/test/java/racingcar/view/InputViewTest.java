66a8de3c63017c0f89c449cac4215d4c394715d7
JMJ <ghtntkdnsk@naver.com>
2025-10-25 18:05:39 +0900

    test: 시도 횟수에 대한 예외 처리 테스트 구현

--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class InputViewTest {
@@ -33,5 +34,29 @@ public class InputViewTest {
 
         Assertions.assertThatThrownBy(() -> inputView.inputCarNames()).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 중복될 수 없습니다.");
+    }
+
+    @Test
+    void 시도_횟수가_숫자가_아닌_경우_예외_처리() {
+        provideInput("abc");
+
+        Assertions.assertThatThrownBy(() -> inputView.inputRoundCount()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도 횟수는 숫자로 입력해야 합니다.");
+    }
+
+    @Test
+    void 시도_횟수가_공백인_경우_예외_처리() {
+        provideInput(" ");
+
+        Assertions.assertThatThrownBy(() -> inputView.inputRoundCount()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도 횟수는 공백일 수 없습니다.");
+    }
+
+    @Test
+    void 시도_횟수가_자연수가_아닌_경우_예외_처리() {
+        provideInput("-1");
+
+        Assertions.assertThatThrownBy(() -> inputView.inputRoundCount()).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("시도 횟수는 자연수여야 합니다.");
     }
 }
